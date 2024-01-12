181cd30f088a34de61b8b0194455b943e9eddb30
Mohammed El Barhichi <mohammed.elbarhichi@centrale-casablanca.ma>
2024-01-12 14:35:50 +0100

    WGP

--- GANRec/losses.py
+++ GANRec/losses.py
@@ -3,8 +3,8 @@ import tensorflow as tf
 def wasserstein_loss(y_true, y_pred):
     return tf.reduce_mean(y_true * y_pred)
 
-def gradient_penalty(real, fake, autoencoder, batch_size):
-    alpha = tf.random_uniform(shape=[batch_size, 1], minval=0., maxval=1.)
+def gradient_penalty(real, fake, autoencoder):
+    alpha = tf.random_uniform(shape=[real.shape[0], 1], minval=0., maxval=1.)
     differences = fake - real
     interpolates = real + (alpha * differences)
     gradients = tf.gradients(autoencoder(interpolates), [interpolates])[0]
@@ -12,6 +12,9 @@ def gradient_penalty(real, fake, autoencoder, batch_size):
     gradient_penalty = tf.reduce_mean((slopes-1.)**2)
     return gradient_penalty
 
-def loss_function(real, fake, autoencoder, batch_size):
+
+def loss_function(real, fake, autoencoder):
     L=10
-    return wasserstein_loss(real, fake) + L * gradient_penalty(real, fake, autoencoder, batch_size)
+    return wasserstein_loss(real, fake) + L * gradient_penalty(real, fake, autoencoder)
+
+
